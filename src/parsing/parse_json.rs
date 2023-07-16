41fef2edcc1c0c08e6adb553ca794f4f159e7156
abourbou <arthuro6996@live.fr>
2023-07-16 17:10:27 +0200

    [feat] Add tests to approve the turing machine validity

--- src/parsing/parse_json.rs
+++ src/parsing/parse_json.rs
@@ -1,6 +1,7 @@
 
 use std::fs;
 use serde::{Deserialize, Serialize};
+extern crate serde_json;
 use serde_json::Value;
 use std::collections::HashMap;
 
@@ -15,19 +16,35 @@ struct JsonMachineDescription {
 	transitions: HashMap<String, Value>,
 }
 
+struct Transition {
+	read : char,
+	to_state : String,
+	write : char,
+	go_right : bool,
+}
+
+struct MachineDescription {
+	name: String,
+	blank: char,
+	initial: String,
+	finals: Vec<String>,
+	list_transitions: HashMap<String, Transition>,
+}
+
 pub fn parse_json(path : &str) {
 
 	let error_path = "Couldn't find or load \"".to_owned() + path + "\" json";
 	let json_str = fs::read_to_string(path)
 						.expect(&error_path);
-	// println!("Open {}", path);
 
 	let json_struct: JsonMachineDescription = serde_json::from_str(&json_str)
 								.expect("Unvalid json data");
-	// println!("data : {:?}", json_struct);
-	// println!("data transition : {:?}", json_struct.transitions);
-	// println!("skip transitions : {:?}", json_struct.transitions["skip"][0]["action"]);
+	check_validity_json(&json_struct);
 
+	println!("Everything works ! (for now...)");
+}
+
+fn check_validity_json(json_struct: &JsonMachineDescription) {
 	check_name(&json_struct.name);
 	check_validy_letter_alphabet(&json_struct.alphabet);
 	check_unicity_alphabet(json_struct.alphabet.as_slice());
@@ -35,8 +52,8 @@ pub fn parse_json(path : &str) {
 	check_unicity_states(json_struct.states.as_slice());
 	check_initial(&json_struct.initial, json_struct.states.as_slice());
 	check_finals(&json_struct.finals, &json_struct.states);
+	check_transitions(&json_struct.alphabet, &json_struct.states, &json_struct.transitions);
 
-	println!("Everything works ! (for now...)");
 }
 
 fn check_name(name : &str) {
@@ -104,5 +121,42 @@ fn check_initial(initial : &String, states : &[String]) {
 fn check_finals(finals : &[String], states : &[String]) {
 	if let Some(bad_final_state) = finals.iter().find(|&final_state| !states.contains(final_state)) {
 		panic!("Final state \"{}\" in not in the states", bad_final_state);
+	}
+}
+
+fn check_transitions(alphabet : &[String], states : &[String], transitions : &HashMap<String, Value>) {
+	transitions.iter().for_each(|transition| {
+		if !states.contains(transition.0) {
+			panic!("Invalid transition \"{}\"", transition.0);
+		}
+
+		let arr_transition = transition.1.as_array().expect("Invalid transition");
+		arr_transition.iter().for_each(|elem_arr_transition| {
+			let obj_transition = elem_arr_transition.as_object().expect("Invalid transition");
+			check_transition(alphabet, states, obj_transition)
+		});
+	});
+}
+
+fn check_transition(alphabet : &[String], states: &[String], transition : &serde_json::Map<String, Value>) {
+	if !transition.contains_key("read") || !transition.contains_key("to_state") ||
+		!transition.contains_key("write") || !transition.contains_key("action"){
+		panic!("Invalid transition");
+	}
+
+	if !alphabet.contains(&transition["read"].as_str().expect("Invalid transition").to_string()){
+		panic!("Invalid transition");
+	}
+
+	if !states.contains(&transition["to_state"].as_str().expect("Invalid transition").to_string()) {
+		panic!("Invalid transition");
+	}
+
+	if !alphabet.contains(&transition["write"].as_str().expect("Invalid transition").to_string()){
+		panic!("Invalid transition");
+	}
+
+	if transition["action"] != "LEFT" && transition["action"] != "RIGHT" {
+		panic!("Invalid transition");
 	}
 }
