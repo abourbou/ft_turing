2dcb74836952f375af4a405c5c4fb78a13bcf246
abourbou <arthuro6996@live.fr>
2023-08-02 18:22:43 +0200

    [feat][refact][style][debug] Fix process turing_machine
    - fix the process with a size larger than the current tape
    - fix blank char added to the tape
    - refact the code to use references
    - add tail recursion crate
    - add colorize crate for print
    - create current state printer function
    - create a machine to check infinite recursion

--- src/process_turing/process.rs
+++ src/process_turing/process.rs
@@ -1,38 +1,48 @@
 
-
 use crate::parsing::parse_json::{MachineDescription, Transition};
+use colored::Colorize;
+use tailcall::tailcall;
 
-pub fn turing_process_rec(tape: String, state: String, desc: MachineDescription, index: i32)
-{
-	if desc.finals.iter().eq([state.clone()].iter()) {
-		return;
-	}
-	println!("[{}] Etat {:?}", tape, state);
-	
-	
-	let read: char = tape.chars().nth(index as usize).unwrap();
-	let tran: Option<&Transition> = desc.list_transitions.get(&state).unwrap()
-							.iter().find(|t: &&Transition| t.read == read);
+pub fn print_state(tape: &str, state: &str, next_tran: &Transition, index: i32) {
+	print!("[");
+	tape.chars().take(index as usize).for_each(|c| print!("{}", c));
+	print!("{}", tape.chars().nth(index as usize).unwrap().to_string().color("red").bold().on_bright_black());
+	tape.chars().skip((index + 1) as usize).for_each(|c| print!("{}", c));
+	print!("]");
 
-	match tran {
-		Some(transition) => {
-			
-			let step = if transition.go_right { 1 } else { -1 };
-			let next_index = index as i32 + step;
-			let new_tape = build_tape(tape.clone(), index as usize, transition.write, next_index.clone());
-
-			if next_index == -1 {
-				turing_process_rec(new_tape, transition.to_state.clone(), desc, 0)
-			}
-			else {
-				turing_process_rec(new_tape, transition.to_state.clone(), desc, next_index)
-			}
-		}
-		None => return ()
-	}
+	let side : &str = if next_tran.go_right{"RIGHT"} else {"LEFT"};
+	println!(" ({}, {}) -> ({}, {}, {})", state, next_tran.read, next_tran.to_state, next_tran.write, side);
 }
 
-pub fn build_tape(tape: String, index_to_change: usize, char_to_replace: char, next_index: i32) -> String {
+#[tailcall]
+pub fn turing_process_rec(tape: String, state: &str, desc: &MachineDescription, index: i32)
+{
+	if desc.finals.iter()
+				  .any(|final_state| state == final_state) {
+		return;
+	}
+
+	let read: char = tape.chars().nth(index as usize).unwrap();
+	let tran: Option<&Transition> = desc.list_transitions.get(state).unwrap()
+							.iter().find(|t: &&Transition| t.read == read);
+
+	if let Some(transition) = tran {
+		print_state(&tape, state, transition, index);
+		let step = if transition.go_right { 1 } else { -1 };
+		let next_index = index + step;
+		let new_tape = build_tape(tape, index as usize, transition.write, next_index, desc);
+
+		if next_index == -1 {
+			turing_process_rec(new_tape, &transition.to_state, desc, 0)
+		}
+		else {
+			turing_process_rec(new_tape, &transition.to_state, desc, next_index)
+		}
+	}
+	println!("No transition for {} with {}", state, read);
+}
+
+pub fn build_tape(tape: String, index_to_change: usize, char_to_replace: char, next_index: i32, desc: &MachineDescription) -> String {
 	let modified_string: String = tape
 		.chars()
 		.enumerate()
@@ -40,20 +50,20 @@ pub fn build_tape(tape: String, index_to_change: usize, char_to_replace: char, n
 		.collect();
 
 	if next_index < 0 {
-		return " ".to_string() + &modified_string;
+		return desc.blank.to_string() + &modified_string;
 	}
-	else if next_index as usize > modified_string.len() { 
-		return modified_string + &" ".to_string();
+	else if next_index as usize >= modified_string.len() {
+		return modified_string + &desc.blank.to_string();
 	}
-		
+
 	modified_string
 }
 
 pub fn process(desc: MachineDescription, tape: &str) {
-	
+
 	println!("{}", desc.initial);
 	println!("{}", tape);
 
 
-	turing_process_rec(tape.to_string(), desc.initial.clone() , desc, 0);
+	turing_process_rec(tape.to_string(), &desc.initial, &desc, 0);
 }
